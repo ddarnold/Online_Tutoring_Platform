1e47128b495bfbf6a06c052a6235523b88c8cee9
joss-thu <antojo01@thu.de>
2024-11-17 13:10:38 +0100

    Dockerization done.

--- backend/src/test/java/de/thu/thutorium/ThutoriumApplicationTests.java
+++ backend/src/test/java/de/thu/thutorium/ThutoriumApplicationTests.java
@@ -1,20 +1,13 @@
 package de.thu.thutorium;
 
-import de.thu.thutorium.controller.SampleController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 @SpringBootTest
 class ThutoriumApplicationTests {
 
-  @Autowired
-  private SampleController sampleController;
+	@Test
+	void contextLoads() {
+	}
 
-  @Test
-  void contextLoads() throws Exception {
-    assertThat(sampleController).isNotNull();
-  }
 }
